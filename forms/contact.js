529fb040aa4234807a0007715f33a633f9be0a13
MuhammadAbiAM <abiadzan15@gmail.com>
2025-04-26 22:52:25 +0700

    Update

--- forms/contact.js
+++ forms/contact.js
@@ -1,18 +1,18 @@
 (function () {
-    emailjs.init('YOUR_PUBLIC_KEY'); // Ganti dengan public key kamu
+    emailjs.init('jm9xImV5A2wLt-7tO');
 })();
 
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('contact-form');
 
     form.addEventListener('submit', function (event) {
-        event.preventDefault(); // Mencegah form submit ke server
+        event.preventDefault();
 
         document.querySelector('.loading').style.display = 'block';
         document.querySelector('.error-message').style.display = 'none';
         document.querySelector('.sent-message').style.display = 'none';
 
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this)
+        emailjs.sendForm('service_bvauzbj', 'template_c7nw0sh', this)
             .then(function () {
                 document.querySelector('.loading').style.display = 'none';
                 document.querySelector('.sent-message').style.display = 'block';
